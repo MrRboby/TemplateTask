a5e917471bfe44cdda0445f9b045240c1316fac8
Alexandr Sergeev <rboby.box@mail.ru>
2020-02-29 16:10:57 +0300

    Implementation of the second task

---
+++ Task2/Task2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "AVLTree.h"
+
+int main() {
+    AVLtree<int> tree = AVLtree<int>(5);
+    tree.insert(10);
+    tree.insert(2);
+    tree.insert(7);
+    tree.insert(15);
+    tree.insert(8);
+    tree.insert(6);
+    tree.insert(1);
+    tree.del(8);
+    int* arr = tree.toArray();
+    for (int i = 0; i < tree.getCount(); i++)
+        std::cout << arr[i] << " ";
+    delete[] arr;
+    return 0;
+}
